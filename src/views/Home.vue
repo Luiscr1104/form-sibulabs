983e935653da2cf97f71cddfb6f3702f7b8756cd
Luis R Zunniga S <luiscr1104@gmail.com>
2021-11-15 08:41:07 -0600

    Front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <FormComponent />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import FormComponent from '../components/FormComponent.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      FormComponent,
+    },
   }
-}
 </script>
